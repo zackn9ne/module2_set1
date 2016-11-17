bf061758488ea2dc2ef8093997b483e9950ad45e
macuser <macuser@Zalbs-MacBook-Air.local>
2016-11-17 16:14:14 -0500

    button logic

--- instructors_demo_codes/semi_advanced_form/app.js
+++ instructors_demo_codes/semi_advanced_form/app.js
@@ -1,18 +1,30 @@
-$( "button" ).on( "click", validate );
+$(function() {
+    console.log("ready"); 
+    
+    var theFormDiv = $( "input" );
 
-function validate(){
-  
-  var thing = $( "input" ).val();
+//* on clicks
+    $( "button" ).on( "click", displayMessage )
+    $( "button" ).on( "click", cleanUp )
+//* on clicks
 
-    console.log(thing);
-
-    if (isNaN(thing)) {
-      $( "div" ).append("chea error nice try");
-      $( "body" ).prepend("stop");
-    }  else {
-      console.log("valid"); 
-      $( "div" ).append("nice job you chose " + thing);
-
-      
+    function cleanUp(){
+	$( "input" ).val('');
     }
-}
+
+    function displayMessage(){
+	
+	var thing = $( "input" ).val();
+
+	console.log(thing);
+
+	if (isNaN(thing)) {
+	    $( "div" ).html("<p>chea error nice try</p>");
+
+	}  else {
+	    console.log("valid"); 
+	    $( "div" ).html("<p>nice job you chose " + thing + "</p>");
+	    
+	}
+    }
+});
